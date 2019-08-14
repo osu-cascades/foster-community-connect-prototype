21ad9dff22ca535b06aa4e6202ee167c822f1296
Shayla Lane <lanesha@oregonstate.edu>
2019-08-13 19:40:27 -0700

    Add webpack config.

---
+++ cofpa-inventory/src/app/app.routing.ts
@@ -0,0 +1,13 @@
+import { Routes, RouterModule } from '@angular/router';
+import { GetComponent } from './get';
+import { AppComponent} from './app.component';
+import { HomeComponent} from './home';
+
+const routes: Routes = [
+    { path: '', component: AppComponent },
+    { path: 'get', component: GetComponent },
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+export const appRoutingModule = RouterModule.forRoot(routes);
