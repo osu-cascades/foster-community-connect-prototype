e6092d1268e540113fb91148afc96026cc62ae9c
Shayla Lane <lanesha@oregonstate.edu>
2019-08-23 15:00:55 -0700

    Log text to console with submit button.

--- cofpa-inventory/src/app/give/give.component.ts
+++ cofpa-inventory/src/app/give/give.component.ts
@@ -1,4 +1,22 @@
 import { Component } from '@angular/core';
+// import { FormControl } from '@angular/forms';
+
 
 @Component({ templateUrl: 'give.component.html' })
-export class GiveComponent {}
+export class GiveComponent {
+    logText(){
+        console.log("clicked");
+        alert("clicked");
+    }
+}
+
+// Reactive form w3 example
+// @Component({
+//     selector: 'app-reactive-favorite-color',
+//     template: `
+//       Favorite Color: <input type="text" [formControl]="favoriteColorControl">
+//     `
+//   })
+//   export class FavoriteColorComponent {
+//     favoriteColorControl = new FormControl('');
+//   }
