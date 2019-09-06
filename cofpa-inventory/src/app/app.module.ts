e3b46488fb45bb23714ad0a904ef5ea9e3171109
Shayla Lane <lanesha@oregonstate.edu>
2019-09-06 09:22:13 -0700

    Add donation form component.

--- cofpa-inventory/src/app/app.module.ts
+++ cofpa-inventory/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { GetComponent } from './get';
 import { GiveComponent } from './give';
 import { HomeComponent } from './home';
+import { DonationFormComponent } from './donation-form/donation-form.component';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { HomeComponent } from './home';
     AppComponent,
     HomeComponent,
     GiveComponent,
-    GetComponent
+    GetComponent,
+    DonationFormComponent
   ],
   imports: [
     BrowserModule,
